2ecfad1b6cae5b4a6303671755f4aa1ee4c231c8
asirobots-open <nate.bunderson@asirobots.com>
2022-04-05 18:19:29 -0600

    Fix broken circle obstacles, make grid inertial

--- asi_ros1_sim/ros2/src/boring_nodes/setup.py
+++ asi_ros1_sim/ros2/src/boring_nodes/setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+from glob import glob
 
 package_name = 'boring_nodes'
 
@@ -7,8 +8,8 @@ setup(
  version='0.0.0',
  packages=[package_name],
  data_files=[
-     ('share/ament_index/resource_index/packages',
-             ['resource/' + package_name]),
+     ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+     ('share/' + package_name+ '/parameter_files', glob('parameter_files/*')),
      ('share/' + package_name, ['package.xml']),
    ],
  install_requires=['setuptools'],
